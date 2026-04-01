799cc694e0befcd0a54a2bdd24c47f2a9f6e3fa3
pablocabe <pablocabe0@gmail.com>
2026-04-01 13:38:34 -0300

    .

---
+++ refactoring/ejercicio06/src/main/java/ar/edu/unlp/info/oo2/refactoring/Ejercicio02.java
@@ -0,0 +1,19 @@
+package ar.edu.unlp.info.oo2.refactoring;
+
+public class Juego {
+    // ......
+}
+
+public class Jugador {
+    private String nombre;
+    private String apellido;
+    private int puntuacion = 0;
+
+    public void incrementarPuntuacion() {
+        this.puntuacion = this.puntuacion + 100;
+    }
+
+    public void decrementarPuntuacion() {
+        this.puntuacion = this.puntuacion - 50;
+    }
+}
