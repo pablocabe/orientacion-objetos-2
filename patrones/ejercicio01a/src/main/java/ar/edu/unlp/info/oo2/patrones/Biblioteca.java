bf2545cad99f144e23bfd61cc748b12a32db267f
pablocabe <pablocabe0@gmail.com>
2026-04-14 09:59:02 -0300

    .

---
+++ patrones/ejercicio01a/src/main/java/ar/edu/unlp/info/oo2/patrones/Biblioteca.java
@@ -0,0 +1,33 @@
+package ar.edu.unlp.info.oo2.patrones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+	private List<Socio> socios;
+	private Exporter exporter;
+
+	public Biblioteca() {
+		socios = new ArrayList<>();
+		exporter= new VoorheesExporter(); 
+	}
+
+	public void agregarSocio(Socio socio) {
+		socios.add(socio);
+	}
+
+	/**
+	 * Retorna la representación JSON de la colección de socios.
+	 */
+	public String exportarSocios() {
+		return exporter.exportar(socios);
+	}
+
+	public Exporter getExporter() {
+		return exporter;
+	}
+
+	public void setExporter(Exporter exporter) {
+		this.exporter = exporter;
+	}
+}
---
+++ patrones/ejercicio01a/src/test/java/ar/edu/unlp/info/oo2/patrones/BibliotecaTest.java
@@ -0,0 +1,6 @@
+package ar.edu.unlp.info.oo2.patrones;
+
+public class BibliotecaTest {
+	// Para completar
+
+}
