aa92003e3ccd884a20361ce0f09c7db4234e5b82
pablocabe <pablocabe0@gmail.com>
2026-04-15 12:49:51 -0300

    .

--- patrones/ejercicio01a/src/test/java/ar/edu/unlp/info/oo2/patrones/BibliotecaTest.java
+++ patrones/ejercicio01a/src/test/java/ar/edu/unlp/info/oo2/patrones/BibliotecaTest.java
@@ -1,6 +1,12 @@
 package ar.edu.unlp.info.oo2.patrones;
 
-public class BibliotecaTest {
-	// Para completar
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.text.ParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.json.simple.parser.JSONParser;
+
+public class BibliotecaTest {
+	
 }
